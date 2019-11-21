f3e536ba68e2fd31de472dc0212f178769581b01
Sven337 <sven337@aquilenet.fr>
2019-11-21 08:20:12 +0100

    fix HTML for roundcube 1.4

--- yubikey.js
+++ yubikey.js
@@ -1,13 +1,14 @@
 if (window.rcmail) {
   rcmail.addEventListener('init', function(evt) {
-    otp_row = '<tr> \
-                 <td class="title"> \
+    otp_row = '<tr class="form-group row"> \
+                 <td class="title" style="display: none;"> \
                    <label for="rcmloginyubikey">' + rcmail.get_label('roundcube_yubikey_authentication.yubikey') + '</label> \
                  </td> \
-                 <td class="input"> \
-                   <input name="_yubikey" style="width: 200px;" id="rcmloginyubikey" autocomplete="off" type="text"> \
+                 <td class="input input-group input-group-lg"> \
+				   <span class="input-group-prepend"><i class="input-group-text">' + rcmail.get_label('roundcube_yubikey_authentication.yubikey') + ' </i></span> \
+				   <input name="_yubikey" style="width: 200px;" id="rcmloginyubikey" autocomplete="off" type="text"> \
                  </td> \
                </tr>';
-    document.getElementsByName('form')[0].getElementsByTagName('table')[0].getElementsByTagName('tbody')[0].innerHTML += otp_row;
+    document.getElementsByName('login-form')[0].getElementsByTagName('table')[0].getElementsByTagName('tbody')[0].innerHTML += otp_row;
   });
 }
